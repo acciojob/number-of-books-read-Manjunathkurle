5055a22f18765f279230474706ad882c1d6baf4e
Manjunathkurle <127331236+Manjunathkurle@users.noreply.github.com>
2023-09-15 12:33:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -18,6 +18,19 @@ const library = [
 
 const numberOfBooksRead = () => {
   // write your code here
+	 let count = 0;
+
+  // Loop through the books in the library
+  for (const book of library) {
+    // Check if the readingStatus is true (indicating the book has been read)
+    if (book.readingStatus === true) {
+      // If true, increment the count
+      count++;
+    }
+  }
+
+  // Return the count of read books
+  return count;
 };
 
 // Do not change the code below
